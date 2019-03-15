edfe235dcde505800c5cd07a9343cd15f0f781a2
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-03-15 10:23:18 +0000

    Fixed tests on step 14 and completed use of double

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,12 @@
 # require_relative 'journey'
 
 class Oystercard
-  attr_reader :balance, :entry_station, :journey_list, :exit_station
+  attr_reader :balance, :journey_list
   DEFAULT_LIMIT = 90
   MINIMUM = 1
 
   def initialize(journey = Journey)
     @balance = 0
-    @entry_station = nil
-    @exit_station = nil
     @journey_list = []
     @journey = journey
   end
@@ -21,18 +19,12 @@ class Oystercard
 
   def touch_in(entry_station)
     raise "Insufficient funds" if @balance < MINIMUM
-    j = @journey.new
-    j.start(entry_station)
-    p j
-    add(j)
-    # add(@journey.new.start(entry_station))
+    journey = @journey.new
+    journey.start(entry_station)
+    add(journey)
   end
 
   def touch_out(exit_station)
-    # @exit_station = exit_station
-    # add_journey
-    # @entry_station = nil
-    p @journey_list
     @journey_list.each do |journey|
       if journey.in_journey? == true
         journey.finish(exit_station)
@@ -41,10 +33,6 @@ class Oystercard
 
     deduct(MINIMUM)
   end
-
-  # def in_journey?
-  #   @entry_station == nil ? false : true
-  # end
 
   private
 
